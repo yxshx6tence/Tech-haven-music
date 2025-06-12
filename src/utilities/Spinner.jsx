17bdc5556ae68810aa0a1c62448e21c5592cb010
Yashwanth S <yashwanthyxsh@gmail.com>
2025-06-12 14:52:44 +0530

    Add files via upload

---
+++ src/utilities/Spinner.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./spinner.css"
+
+const Spinner = () => {
+  return (
+    <section className='h-[100vh] w-[100%] bg-[#00000066] fixed top-0 left-0'>
+<div id="container">
+  <div className="spinner">
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</div>
+      
+    </section>
+  )
+}
+
+export default Spinner
